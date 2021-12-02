f659a758bead9489ed8d68ca379aa812862890d9
spidero7 <pajak.bartlomiej97@gmail.com>
2021-12-02 20:40:36 +0100

    Create two function (hall of fame)

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,45 @@
+import { doc } from "prettier"
+
 export const App = ({options}) => {
 }
 
+const hallOfFameContainer = document.querySelector('.swquiz-app-hall-of-fame-container');
 
+function showHallOfFame() {
+    const headerRanking = document.createElement('h2');
+    headerRanking.innerHTML = "Ranking";
+    headerRanking.classList.add('swquiz-app-hall-of-fame-ranking-header');
+    hallOfFameContainer.appendChild(headerRanking);
+
+
+    const hallOfFameHeaderDiv = document.createElement('div');
+    hallOfFameHeaderDiv.classList.add('swquiz-app-hall-of-fame-header');
+
+    hallOfFameHeaderDiv.innerHTML = 
+        `<p>Place</p>
+        <p>Payer</p>
+        <p>Scores</p>`;
+
+    hallOfFameContainer.appendChild(hallOfFameHeaderDiv);
+
+
+    const hallOfFamePlayerDiv = document.createElement('div');
+    hallOfFamePlayerDiv.classList.add('swquiz-app-hall-of-fame-player');
+
+    hallOfFamePlayerDiv.innerHTML = 
+        `<p>1st</p>
+        <p>Bartek</p>
+        <p>15/20</p>`;
+
+    hallOfFameContainer.appendChild(hallOfFamePlayerDiv);
+}
+
+// showHallOfFame();
+// showTextIfNoUserScore();
+
+function showTextIfNoUserScore() {
+    const emptyHallOfFame = document.createElement('h1');
+    emptyHallOfFame.innerHTML = 'Hall of Fame is empty';
+    emptyHallOfFame.classList.add('swquiz-app-hall-of-fame-empty')
+    hallOfFameContainer.appendChild(emptyHallOfFame);
+}
