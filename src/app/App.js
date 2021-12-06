3b1e8b7ad92e5f1abb0f61a01cbe0765384fc79c
Justyna <justyna.chmielinska@coderscrew.pl>
2021-12-06 18:22:07 +0100

    Przykładowy obrazek do pytania onLoad

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,10 @@
 export const App = ({options}) => {
+     // Wyświetlanie obrazka po lewej stronie
+     function showPic(mode, id) {
+        document.getElementById("left-pic").src=`../static/assets/img/modes/${mode}/${id}.jpg`
+    }
+    
+    showPic(options.mode, options.id);
 }
 
 
