6b9ba2c94e4ac6ed432415cd47ef26ec793c8ae7
spidero7 <pajak.bartlomiej97@gmail.com>
2021-12-09 20:44:22 +0100

    Scores in order from largest to smallest

--- src/components/HallOfFame.js
+++ src/components/HallOfFame.js
@@ -6,20 +6,41 @@ const hallOfFameContainer = document.querySelector(
 // Draft data
 const usersScores = [
   { playerName: 'Luke', score: 18 },
-  { playerName: 'Han', score: 15 },
+  { playerName: 'Han', score: 20 },
   { playerName: 'Vader', score: 10 },
+  { playerName: 'Obi', score: 16 },
 ];
+
+// Scores in order from largest to smallest
+const arrayOfScores = usersScores.map((x) => x.score);
+arrayOfScores.sort(function (a, b) {
+  return b - a;
+});
+
+console.log(arrayOfScores);
+
+// let threeHighestScores = 2;
+
+// for (let i = 0; i <= threeHighestScores; i++) {
+//   if (arrayOfScores[i] > threeHighestScores) {
+//     let threeHighestScores = arrayOfScores[i];
+//     threeHighestScores.map((x) => x);
+//   }
+//   console.log(threeHighestScores);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   const threeHighestScores = arrayOfScores[i];
+//   console.log(threeHighestScores);
+// }
 
 // Adding player name and score to Local Storage
 window.localStorage.setItem('user', JSON.stringify(usersScores));
 
-// Data from Local Storage
-const userData = JSON.parse(window.localStorage.getItem('user'));
-
-// console.log(userData);
-const scores = userData.map((x) => x.score);
-
-console.log(userData);
+// Getting the data from Local Storage
+const userDataFromLocalStorage = JSON.parse(
+  window.localStorage.getItem('user'),
+);
 
 // Showing Hall of Fame ranking if user've played
 function showHallOfFame() {
@@ -41,26 +62,25 @@ function showHallOfFame() {
     const hallOfFamePlayerDiv = document.createElement('div');
     hallOfFamePlayerDiv.classList.add('swquiz-app-hall-of-fame-player');
 
+    const scores = userDataFromLocalStorage.map((x) => x.score);
+
     const highestScore = Math.max(...scores);
     const lowestScore = Math.min(...scores);
 
     if (scores[i] === highestScore) {
-      userData[i]['place'] = 1;
+      userDataFromLocalStorage[i]['place'] = 1;
     } else if (scores[i] === lowestScore) {
-      userData[i]['place'] = 3;
+      userDataFromLocalStorage[i]['place'] = 3;
     } else {
-      userData[i]['place'] = 2;
+      userDataFromLocalStorage[i]['place'] = 2;
     }
 
-    hallOfFamePlayerDiv.innerHTML = `<p>${userData[i].place}</p>
-          <p>${userData[i].playerName}</p>
-          <p>${userData[i].score}</p>`;
+    hallOfFamePlayerDiv.innerHTML = `<p>${userDataFromLocalStorage[i].place}</p>
+          <p>${userDataFromLocalStorage[i].playerName}</p>
+          <p>${userDataFromLocalStorage[i].score}</p>`;
     hallOfFameContainer.appendChild(hallOfFamePlayerDiv);
   }
 }
-
-showHallOfFame();
-// showTextIfNoUserScore();
 
 // Showing text if user haven't played
 function showTextIfNoUserScore() {
@@ -69,3 +89,6 @@ function showTextIfNoUserScore() {
   emptyHallOfFame.classList.add('swquiz-app-hall-of-fame-empty');
   hallOfFameContainer.appendChild(emptyHallOfFame);
 }
+
+showHallOfFame();
+// showTextIfNoUserScore();
