f7c94ff24923054e0fdcfa03ac38e93da9f88593
spidero7 <pajak.bartlomiej97@gmail.com>
2021-12-20 13:06:35 +0100

    Added mode to function

--- src/components/HallOfFame.js
+++ src/components/HallOfFame.js
@@ -3,11 +3,12 @@ const hallOfFameContainer = document.querySelector('.quiz-game-ranking');
 
 // Draft data
 const usersScores = [
-  { playerName: 'Luke', score: 30, answers: 50 },
-  { playerName: 'Han', score: 18, answers: 80 },
-  { playerName: 'Leia', score: 18, answers: 100 },
-  { playerName: 'Yoda', score: 4, answers: 4 },
-  { playerName: 'Chewy', score: 15, answers: 16 },
+  { playerName: 'Luke', score: 30, answers: 50, mode: 'people' },
+  { playerName: 'Han', score: 18, answers: 80, mode: 'vehicles' },
+  { playerName: 'R2D2', score: 18, answers: 80, mode: 'vehicles' },
+  { playerName: 'Leia', score: 18, answers: 100, mode: 'people' },
+  { playerName: 'Yoda', score: 4, answers: 4, mode: 'starship' },
+  { playerName: 'Chewy', score: 15, answers: 16, mode: 'people' },
 ];
 
 usersScores.map((player) => {
@@ -23,6 +24,22 @@ function showHallOfFame() {
   // Getting the data from Local Storage
   let userDataFromLocalStorage = JSON.parse(localStorage.getItem('user'));
 
+  // Mode variables
+  const people = userDataFromLocalStorage.filter((items) => {
+    return items.mode == 'people';
+  });
+
+  const vehicles = userDataFromLocalStorage.filter((items) => {
+    return items.mode == 'vehicles';
+  });
+
+  const starship = userDataFromLocalStorage.filter((items) => {
+    return items.mode == 'starship';
+  });
+
+  // Switch mode
+  const mode = people;
+
   const headerRanking = document.createElement('h2');
   headerRanking.innerHTML = 'Mode Ranking';
   headerRanking.classList.add('swquiz-app-hall-of-fame-ranking-header');
@@ -37,25 +54,27 @@ function showHallOfFame() {
 
   hallOfFameContainer.appendChild(hallOfFameHeaderDiv);
 
-  // Sort by percentage
-  userDataFromLocalStorage.sort((a, b) =>
-    a.percentage < b.percentage ? 1 : a.percentage > b.percentage ? -1 : 0,
-  );
-
-  // Score variable
-  const threeHighestScore = userDataFromLocalStorage.slice(0, 3);
-
   for (let i = 0; i < userDataFromLocalStorage.length; i++) {
-    const hallOfFamePlayerDiv = document.createElement('div');
-    hallOfFamePlayerDiv.classList.add('swquiz-app-hall-of-fame-player');
+    if (true) {
+      // Sort by percentage
+      mode.sort((a, b) =>
+        a.percentage < b.percentage ? 1 : a.percentage > b.percentage ? -1 : 0,
+      );
 
-    userDataFromLocalStorage[i].place = i + 1;
+      // Score variable
+      const threeHighestScore = mode.slice(0, 3);
 
-    if (threeHighestScore) {
-      hallOfFamePlayerDiv.innerHTML = `<p>${threeHighestScore[i].place}</p>
+      const hallOfFamePlayerDiv = document.createElement('div');
+      hallOfFamePlayerDiv.classList.add('swquiz-app-hall-of-fame-player');
+
+      mode[i].place = i + 1;
+
+      if (threeHighestScore) {
+        hallOfFamePlayerDiv.innerHTML = `<p>${threeHighestScore[i].place}</p>
           <p>${threeHighestScore[i].playerName}</p>
           <p>${threeHighestScore[i].score} / ${threeHighestScore[i].answers}</p>`;
-      hallOfFameContainer.appendChild(hallOfFamePlayerDiv);
+        hallOfFameContainer.appendChild(hallOfFamePlayerDiv);
+      }
     }
   }
 }
