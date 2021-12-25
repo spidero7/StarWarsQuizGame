1900749a5f1729a1b58fbfea75440ccb742753a8
WojtekPugowski <wojtek@pugowski.pl>
2021-12-25 20:39:23 +0100

    change shadow in progress ligthsaber path

--- src/app/wp-lightsaber.js
+++ src/app/wp-lightsaber.js
@@ -1,5 +1,6 @@
 export function progressSword() {
   document.querySelector('.quiz-progress-lightsaber__animation').setAttribute("id", "start-animation");
   const startAnimation = document.querySelector('.quiz-progress-lightsaber__firebrand');
-  startAnimation.style.backgroundColor = 'rgba(255, 255, 255, 0.9 )';
+  startAnimation.style.backgroundColor = 'rgba(255, 255, 255, 0.5 )';
+  startAnimation.style.boxShadow = 'inset 0 0 0 #000000';
 }
