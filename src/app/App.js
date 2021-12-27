c3166c75cf69aaa584e0f3948f8f630844c10adb
Justyna <justyna.chmielinska@coderscrew.pl>
2021-12-27 21:25:09 +0100

    adjusted CSS

--- src/app/App.js
+++ src/app/App.js
@@ -7,6 +7,6 @@ export const App = ({ options }) => {
     window.usedQuestionIds = [] // after finished game, this global should be set to empty array + it is used for storing used questions info
     window.precachedQuestions = [] // after finished game, this global should be set to empty array + it is used for precaching questions
     window.cachedQuestionsPromises = [] // after finished game, this global should be set to empty array + it is used for limiting the amount of fetches
-    QuestionsView(document.querySelector('.quiz-game-questions'), "starships", true)
+    QuestionsView(document.querySelector('.quiz-game-questions'), "vehicles", true)
     leftPicture(window.currentQuestion.mode, window.currentQuestion.id)
 }
