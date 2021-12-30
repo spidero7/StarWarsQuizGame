8a3431552c189476b3f9bd3ccecf2ad33780db2e
JK-Sebastiao <kiakumbo.9@gmail.com>
2021-12-31 00:36:20 +0100

    fix: disable APP call and fix hall of fame for loop

--- src/index.js
+++ src/index.js
@@ -15,4 +15,4 @@ document
 // const SW_API_BASE_URL = process.env.SW_API_BASE_URL || "https://swapi.dev/api";
 // const QUIZ_MAX_TIME = process.env.QUIZ_MAX_TIME_SECONDS ? process.env.QUIZ_MAX_TIME_SECONDS * ONE_SECOND_MILLIS : 120 * ONE_SECOND_MILLIS;
 
-window.onload = () => App({options: {swApiBaseUrl: SW_API_BASE_URL, quizMaxTime: QUIZ_MAX_TIME}})
+//window.onload = () => App({options: {swApiBaseUrl: SW_API_BASE_URL, quizMaxTime: QUIZ_MAX_TIME}})
