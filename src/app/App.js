44f7ab81f3a7da0407b7c36c6ffe46d90ff27164
itspatys <patryk.pierzchaa@gmail.com>
2022-01-03 12:15:19 +0100

    Połączyłem pytania z progress barem i timerem. Dodałem przyciski do rozpoczącia gry, hall of fame i rules.

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,15 @@
 import { doc } from 'prettier';
 import { selectMode } from "./mode-selection.js"
 import { leftPicture } from '../components/images'
-import QuestionsView from "../components/QuestionsView.js"
+import GameButtonsView from "../components/GameButtonsView"
 export const App = ({ options }) => {
     // window.askedIds = [] // for debuging
     window.currentPoints = { points: 0, askedQuestions: 0 }
-    window.currentQuestion = { mode: "starships", id: 11 }
+    window.currentQuestion = { mode: "people", id: 36 }
     window.usedQuestionIds = [] // after finished game, this global should be set to empty array + it is used for storing used questions info
     window.precachedQuestions = [] // after finished game, this global should be set to empty array + it is used for precaching questions
     window.cachedQuestionsPromises = [] // after finished game, this global should be set to empty array + it is used for limiting the amount of fetches
-    QuestionsView(document.querySelector('.quiz-game-questions'), "vehicles", true)
+    window.chosenGameMode = "people" // TODO: should update after chosing different gamemodes
+    GameButtonsView("rules")
     leftPicture(window.currentQuestion.mode, window.currentQuestion.id)
 }
